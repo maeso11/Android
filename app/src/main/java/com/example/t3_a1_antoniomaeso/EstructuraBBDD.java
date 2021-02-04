98c5845b730237967ddcb1e8781add366860fdeb
maeso <tonymaeso11@gmail.com>
2021-02-04 09:28:41 +0100

    Correccion estructura de la base de datos

--- app/src/main/java/com/example/t3_a1_antoniomaeso/EstructuraBBDD.java
+++ app/src/main/java/com/example/t3_a1_antoniomaeso/EstructuraBBDD.java
@@ -6,21 +6,22 @@ public class EstructuraBBDD {
 
     public static final String SQL_CREATE_PARTIDAS =
             "CREATE TABLE IF NOT EXISTS " + EstructuraPartidas.TABLE_NAME_PARTIDAS + "(" +
-            EstructuraPartidas._ID + " integer PRIMARY KEY, " +
-            EstructuraPartidas.COLUMN_NAME_GANADOR + " text);"
+                    EstructuraPartidas._ID + " integer PRIMARY KEY, " +
+                    EstructuraPartidas.COLUMN_NAME_GANADOR + " text);"
 
-}
 
-private EstructuraBBDD(){}
+    private EstructuraBBDD() {
+    }
 
-public static class EstructuraPartidas implements BaseColumns {
-    public static final String TABLE_NAME_PARTIDAS = "partidas";
-    public static final String COLUMN_NAME_GANADOR = "ganador";
-}
+    public static class EstructuraPartidas implements BaseColumns {
+        public static final String TABLE_NAME_PARTIDAS = "partidas";
+        public static final String COLUMN_NAME_GANADOR = "ganador";
+    }
 
-public static class EstructuraUsuarios implements BaseColumns{
-    public static final String TABLE_NAME_USUARIOS = "usuarios";
-    public static final String COLUMN_NAME_NOMBRE = "nombre";
-    public static final String COLUMN_NAME_PUNTOS = "puntos";
-    public static final String COLUMN_NAME_CANTIDAD = "partidas jugadas";
+    public static class EstructuraUsuarios implements BaseColumns {
+        public static final String TABLE_NAME_USUARIOS = "usuarios";
+        public static final String COLUMN_NAME_NOMBRE = "nombre";
+        public static final String COLUMN_NAME_PUNTOS = "puntos";
+        public static final String COLUMN_NAME_CANTIDAD = "partidas jugadas";
+    }
 }
